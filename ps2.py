d1aa069d11e979affaa1083eb65c4ca9a02dd416
2018JTM2003 <poojajharwal697@gmail.com>
2018-09-27 15:40:39 +0530

    updated file

--- ps2.py
+++ ps2.py
@@ -1,14 +1,22 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
-s = raw_input("enter the string here")
-k1= int(input("enter the value of key1="))
-k2= int(input("enter the value of key2="))
-k3= int(input("enter the value of key3="))
-l= len(s)
+string = raw_input("enter the string here")
+key1= int(input("enter the value of key1="))
+key2= int(input("enter the value of key2="))
+key3= int(input("enter the value of key3="))
+length= len(s)
 from array import *
-a1= array('c', [])
-a2= array('c', [])
-a3= array('c', [])
-for i in range(l):
-
+a= array('c', [])
+b= array('c', [])
+c= array('c', [])
+for i in range(length):
+if(string(i)>= 'a' and string(i)<='i'):
+a.append(string(i))
+if(string(i)>= 'j' and string(i)<='r'):
+b.append(string(i))
+if(string(i)>= 's' and string(i) <='z' and string(i)== '_'):
+c.append(string(i))
+print a
+print b
+print c

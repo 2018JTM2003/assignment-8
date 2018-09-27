8eba2b93880e8f79a87a2ae6f6e8e7eb2043cd1e
2018JTM2003 <poojajharwal697@gmail.com>
2018-09-27 16:48:18 +0530

    updated file

--- ps2.py
+++ ps2.py
@@ -5,21 +5,22 @@
 #!/usr/bin/python
 
 string = raw_input("enter the string here")  # Takes the string as input
-key1= int(input("enter the value of key1=")) # takes the valus of key1
+ckey1= int(input("enter the value of key1=")) # takes the valus of key1
 key2= int(input("enter the value of key2=")) # takes the valus of key2
 key3= int(input("enter the value of key3=")) # takes the valus of key3
-length= len(s) # it tarcks the length of the string
+length= len(string) # it tarcks the length of the string
 from array import *
 a= array('c', []) #appends the first character of the string 
 b= array('c', [])
 c= array('c', [])
 for i in range(length):   #using the for loop
-if string(i)>= 'a' and string(i)<='i' : # defining conditions
-a.append(string(i))
-if string(i)>= 'j' and string(i)<='r' :
-b.append(string(i))
-if string(i)>= 's' and string(i) <='z' and string(i)== '_':
-c.append(string(i))
+ if string(i)>= 'a' and string(i)<='i': # defining conditions
+    a.append(string(i))
+ if string(i)>= 'j' and string(i)<='r':
+    b.append(string(i))
+ if string(i)>= 's' and string(i) <='z' and string(i)== '_':
+    c.append(string(i))
+
 print a #prints value of a
 print b #prints value of a
 print c #prints value of c

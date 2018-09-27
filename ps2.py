9490753a6e0c86458736f229c5a14da4d981c4c1
2018JTM2003 <poojajharwal697@gmail.com>
2018-09-27 15:14:01 +0530

    updated file

--- ps2.py
+++ ps2.py
@@ -1,3 +1,14 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+s = raw_input("enter the string here")
+k1= int(input("enter the value of key1="))
+k2= int(input("enter the value of key2="))
+k3= int(input("enter the value of key3="))
+l= len(s)
+from array import *
+a1= array('c', [])
+a2= array('c', [])
+a3= array('c', [])
+for i in range(l):
+

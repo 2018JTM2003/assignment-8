fa7881aff374c7a484e3f37ae3ac4d54b0d2f414
2018JTM2003 <poojajharwal697@gmail.com>
2018-09-27 17:03:12 +0530

    updated file

--- ps1.py
+++ ps1.py
@@ -1,3 +1,11 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+
+#!/usr/bin/python
+
+a= int(input("enter the value of m ")) # takes the valus of m 
+b= int(input("enter the value of n")) # takes the valus of n
+for a in range(2,105):     #to iterate between 2 to 105
+   for b in range(2,105): #to iterate between 2 to 105
